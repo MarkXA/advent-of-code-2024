0690b5ffa1f2b17961b206296ab8b69b8bac61ea
Mark Allan <markxa@markxa.com>
2024-12-08 08:58:11 +0000

    Day 8

--- 2DStuff.cs
+++ 2DStuff.cs
@@ -1,19 +1,27 @@
 namespace AdventOfCode2024;
 
 public enum Direction { Up = 0, Right = 1, Down = 2, Left = 3 }
-public record Movement(int X, int Y);
+
+public record Delta(int X, int Y)
+{
+    public static Delta operator -(Delta d) => new(-d.X, -d.Y);
+}
+
 public record Location(int X, int Y)
 {
-    private static readonly Dictionary<Direction, Movement> Movements = new()
+    private static readonly Dictionary<Direction, Delta> DirectionDeltas = new()
     {
-        [Direction.Up] = new Movement(0, -1),
-        [Direction.Down] = new Movement(0, 1),
-        [Direction.Left] = new Movement(-1, 0),
-        [Direction.Right] = new Movement(1, 0)
+        [Direction.Up] = new Delta(0, -1),
+        [Direction.Down] = new Delta(0, 1),
+        [Direction.Left] = new Delta(-1, 0),
+        [Direction.Right] = new Delta(1, 0)
     };
 
-    public static Location operator +(Location l, Movement m) => new Location(l.X + m.X, l.Y + m.Y);
-    public static Location operator +(Location l, Direction d) => l + Movements[d];
+    public static Location operator +(Location l, Delta d) => new(l.X + d.X, l.Y + d.Y);
+    public static Location operator -(Location l, Delta d) => new(l.X - d.X, l.Y - d.Y);
+    public static Location operator +(Location l, Direction d) => l + DirectionDeltas[d];
+    public static Delta operator -(Location l1, Location l2) => new(l1.X - l2.X, l1.Y - l2.Y);
+
     public bool Inside<T>(List<List<T>> map) => Y >= 0 && Y < map.Count && X >= 0 && X < map[Y].Count;
 }
 
---
+++ Day8.cs
@@ -0,0 +1,66 @@
+﻿namespace AdventOfCode2024;
+
+public partial class Day8 : IPuzzle
+{
+    public record Antenna(char Code, Location Location);
+
+    private List<List<char>> map = [];
+    private IList<(Location loc1, Location loc2)> antennaPairs = [];
+
+    public void LoadInput(string inputPath)
+    {
+        map = File.ReadAllLines(inputPath).RemoveEmpty().Select(s => s.ToList()).ToList();
+
+        antennaPairs = map.Index().SelectMany(
+            row => row.Item.Index().Select(
+                col => new Antenna(col.Item, new Location(col.Index, row.Index))))
+            .Where(a => a.Code != '.')
+            .GroupBy(a => a.Code)
+            .Select(g => g.ToList())
+            .Select(g => g
+                .Index()
+                .SelectMany(g1 =>
+                    g.Skip(g1.Index + 1)
+                    .Select(g2 => (loc1: g1.Item.Location, loc2: g2.Location))))
+            .SelectMany(p => p)
+            .ToList();
+    }
+
+    public long Part1()
+    {
+        HashSet<Location> antinodes = [];
+
+        foreach (var (loc1, loc2) in antennaPairs)
+        {
+            var delta = loc2 - loc1;
+            antinodes.Add(loc1 - delta);
+            antinodes.Add(loc2 + delta);
+        }
+
+        return antinodes.Where(l => l.Inside(map)).Count();
+    }
+
+    public long Part2()
+    {
+        HashSet<Location> antinodes = new();
+
+        foreach (var (loc1, loc2) in antennaPairs)
+        {
+            var delta = loc2 - loc1;
+            var antinode = loc1;
+            while (antinode.Inside(map))
+            {
+                antinodes.Add(antinode);
+                antinode -= delta;
+            }
+            antinode = loc2;
+            while (antinode.Inside(map))
+            {
+                antinodes.Add(antinode);
+                antinode += delta;
+            }
+        }
+
+        return antinodes.Count();
+    }
+}

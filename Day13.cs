81c7b07961df43c31542c2940757182d15325cc4
Mark Allan <markxa@markxa.com>
2024-12-13 22:01:05 +0000

    Day 13 tidying

--- Day13.cs
+++ Day13.cs
@@ -16,7 +16,7 @@ public partial class Day13 : IPuzzle
 
         equations = input.Chunk(4).Select(lines =>
         {
-            var xys = lines.Select(l => matchInt.Matches(l).Select(m => long.Parse(m.Value))).SelectMany(x => x).ToArray();
+            var xys = lines.Select(l => matchInt.Matches(l).Select(m => long.Parse(m.Value))).SelectMany(n => n).ToArray();
             return new Equation(xys[0], xys[1], xys[2], xys[3], xys[4], xys[5]);
         });
     }
@@ -39,7 +39,7 @@ public partial class Day13 : IPuzzle
 
     public long Part2()
     {
-        equations = equations.Select(eq => new Equation(eq.XA, eq.YA, eq.XB, eq.YB, eq.XT + 10000000000000, eq.YT + 10000000000000)).ToList();
+        equations = equations.Select(eq => new Equation(eq.XA, eq.YA, eq.XB, eq.YB, eq.XT + 10000000000000, eq.YT + 10000000000000));
         return equations.Select(Solve).Sum(s => s.a * 3 + s.b);
     }
 }
